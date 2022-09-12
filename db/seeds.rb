64d938805a29f77d4eb18cef00474c5a28ef6a63
butinhina <butinhina113@gmail.com>
2022-09-12 15:13:39 +0000

    [db:seed追加]

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,7 +24,23 @@ Tag.create([
   { name: 'コース'}
   ])
 
-  Admin.create!(
-    email: 'a@a',
-    password: 'aaaaaa'
+Admin.create(
+  email: 'a@a',
+  password: 'aaaaaa'
   )
+
+customers = Customer.create(
+  [
+    {email: 'o@o', last_name: 'Olivia', first_name: 'hinaka', last_name_kana: 'オリビア', first_name_kana: 'ヒナカ', password: 'oooooo', nickname: 'hinapon', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/sample-user3.jpg"), filename:"sample-user3.jpg")},
+    {email: 'j@j', last_name: 'James', first_name: 'hitoshi', last_name_kana: 'ジェイムズ', first_name_kana: 'ヒトシ', password: 'jjjjjj', nickname: 'zin', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/sample-user2.jpg"), filename:"sample-user2.jpg")},
+    {email: 'l@l', last_name: 'Lucas', first_name: 'yosuke', last_name_kana: 'ルカズ', first_name_kana: 'ヨウスケ', password: 'llllll', nickname: 'suke', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/sample-user1.jpg"), filename:"sample-user1.jpg")}
+  ]
+)
+
+# PostVideo.create(
+#   [
+#     {report: '2022.03.28 ひなぽんスウィング', customer_id: customers[0].id, status: "published" },
+#     {report: '2020.08.05 じんスウィング', customer_id: customers[1].id, status: "published" },
+#     {report: '2021.09.21 すけスウィング', customer_id: customers[2].id, status: "published" }
+#   ]
+# )

77adf720efbe32f18251e824c231762a49920a06
butinhina <butinhina113@gmail.com>
2022-09-15 05:06:31 +0000

    [profile_imageバリデーション変更]

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -19,7 +19,7 @@ class Customer < ApplicationRecord
   validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :nickname, presence: true
-  validate  :profile_image_type
+  # validate  :profile_image_type
 
   def get_profile_image(width,height)
   unless profile_image.attached?
@@ -40,7 +40,7 @@ class Customer < ApplicationRecord
     end
   end
 
-  private
+  # private
 
   # def profile_image_size
   #   if profile_image.attach.byte_size > 1.megabytes
@@ -48,9 +48,9 @@ class Customer < ApplicationRecord
   #   end
   # end
 
-  def profile_image_type
-    if profile_image.attach.content_type.in?(%('image/jpg image/png'))
-      errors.add(:profile_image, 'はjpegまたはpng形式でアップロードしてください')
-    end
-  end
+  # def profile_image_type
+  #   if profile_image.attach.content_type.in?(%('image/jpg image/png'))
+  #     errors.add(:profile_image, 'はjpegまたはpng形式でアップロードしてください')
+  #   end
+  # end
 end

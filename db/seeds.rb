b5ad9ee12407df5f06f7bff8157e8b69e41f8e09
butinhina <butinhina113@gmail.com>
2022-09-15 01:02:30 +0000

    [seed内容変更]

--- db/seeds.rb
+++ db/seeds.rb
@@ -24,20 +24,20 @@ Tag.create([
   { name: 'コース'}
   ])
 
-Admin.create(
+Admin.create!(
   email: 'a@a',
   password: 'aaaaaa'
   )
 
-customers = Customer.create(
+customers = Customer.create!(
   [
-    {email: 'o@o', last_name: 'Olivia', first_name: 'hinaka', last_name_kana: 'オリビア', first_name_kana: 'ヒナカ', password: 'oooooo', nickname: 'hinapon', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/ひなぽん.jpg"), filename:"ひなぽん.jpg")},
-    {email: 'j@j', last_name: 'James', first_name: 'hitoshi', last_name_kana: 'ジェイムズ', first_name_kana: 'ヒトシ', password: 'jjjjjj', nickname: 'zin', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/じんくん.jpg"), filename:"じんくん.jpg")},
-    {email: 'l@l', last_name: 'Lucas', first_name: 'yosuke', last_name_kana: 'ルカズ', first_name_kana: 'ヨウスケ', password: 'llllll', nickname: 'suke', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/すけ.jpg"), filename:"すけ.jpg")}
+    {email: 'o@o', last_name: 'Olivia', first_name: 'hinaka', last_name_kana: 'オリビア', first_name_kana: 'ヒナカ', password: 'oooooo', nickname: 'hinapon', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/ひなぽん.jpeg"), filename:"ひなぽん.jpeg")},
+    {email: 'j@j', last_name: 'James', first_name: 'hitoshi', last_name_kana: 'ジェイムズ', first_name_kana: 'ヒトシ', password: 'jjjjjj', nickname: 'zin', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/じんくん.jpeg"), filename:"じんくん.jpeg")},
+    {email: 'l@l', last_name: 'Lucas', first_name: 'yosuke', last_name_kana: 'ルカズ', first_name_kana: 'ヨウスケ', password: 'llllll', nickname: 'suke', profile_image: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/profile_image/すけ.jpeg"), filename:"すけ.jpeg")}
   ]
 )
 
-PostVideo.create(
+PostVideo.create!(
   [
     {report: '2022.03.28 ひなぽんスウィング', customer_id: "1", video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/swings/ひなぽんスウィング.mp4"), filename:"ひなぽんスウィング.mp4")},
     {report: '2020.08.05 じんスウィング', customer_id: "2", video: ActiveStorage::Blob.create_and_upload!(io: File.open("#{Rails.root}/db/swings/じんくん後方.mp4"), filename:"じんくん後方.mp4")},

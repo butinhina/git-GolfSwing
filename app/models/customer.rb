cbdb141fbdd95f07a91fd01fb140abbc86ed47d6
butinhina <102806973+butinhina@users.noreply.github.com>
2022-12-13 17:12:15 +0000

    top画面編集

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -16,6 +16,7 @@ class Customer < ApplicationRecord
   # validates :last_name_kana, presence: true
   # validates :first_name, presence: true
   # validates :first_name_kana, presence: true
+  validates :password, presence: true
   validates :email, presence: true
   validates :encrypted_password, presence: true
   validates :nickname, presence: true
